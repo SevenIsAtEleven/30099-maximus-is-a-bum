c6f980a9af48b46096ef4cc7874e42977736ebc2
maximusxiao <136401797+maximusxiao@users.noreply.github.com>
2025-07-03 16:58:39 -0700

    Update robot.java

--- robot.java
+++ robot.java
@@ -4,16 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class robot {
-    public void drive(DcMotor motor_f_l, DcMotor motor_b_l, DcMotor motor_f_r, DcMotor motor_b_r) {
+    public void drive(Gamepad gamepad1,DcMotor motor_f_l, DcMotor motor_b_l, DcMotor motor_f_r, DcMotor motor_b_r) {
         // y_val reversed
         double y = -gamepad1.left_stick_y;
-        
+
         // Multiplier of 1.17 to counteract strafing range
         double x = gamepad1.left_stick_x * 1.17;
         double rx = gamepad1.right_stick_x;
-
         // Range_limiter containes power within range [-1,1]
         double range_limiter = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double power_fl = -1 * ((y + x + rx) / range_limiter);
