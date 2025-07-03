cc79e6145fe5412bc68045ff7cf427819c736c01
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-03 08:17:35 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,19 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
 import manual_functions.robot
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+    
 @TeleOp()
 public class main extends OpMode {
     private robot obj;
     
     @Override
     public void init() {
-        obj.initialize()
+        obj.initialize(HardwareMap)
     }
     @Override
     public void loop() {
