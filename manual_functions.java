798ed1e430842c8a620947820842ad1e0f745aef
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-02 18:50:29 -0700

    Update and rename functions.java to manual_functions.java

---
+++ manual_functions.java
@@ -0,0 +1,26 @@
+public class robot {
+    public void initialize() {
+        
+    }
+    public void Mechanum_drive() {
+
+    }
+    public void spin_intake() {
+
+    }
+    public void horizontal_expansion() {
+    
+    }
+    public void intake_box() {
+
+    }
+    public void claw(open) {
+
+    }
+    public void vertical_expansion() {
+
+    }
+    public void swing() {
+
+    }
+}
