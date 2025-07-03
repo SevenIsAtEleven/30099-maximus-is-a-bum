0e2173c3a479eb3fe4329cad4c956a207dd01703
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-02 19:00:16 -0700

    Update manual_functions.java

--- manual_functions.java
+++ manual_functions.java
@@ -2,10 +2,18 @@ public class robot {
     public void initialize() {
         telemetry.addData("Sys", "Starting initialization process")
 
-        DcMotor motor_f_l = 
-        DcMotor motor_f_l
-        DcMotor motor_f_l
-        DcMotor motor_f_l
+        DcMotor motor_f_l;
+		DcMotor motor_f_r;
+		DcMotor motor_b_l;
+		DcMotor motor_b_r;
+		motor_f_l = hwMap.get(DcMotor.class, "motor_f_l");
+		motor_f_l.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		motor_f_r = hwMap.get(DcMotor.class, "motor_f_r");
+		motor_f_r.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		motor_b_l = hwMap.get(DcMotor.class, "motor_b_l");
+		motor_b_l.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		motor_b_r = hwMap.get(DcMotor.class, "motor_b_r");
+		motor_b_r.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         telemetry.addData("Sys", "Drive motor initialization process complete")
         
         telemetry.addData("Sys", "Starting initialization process")
