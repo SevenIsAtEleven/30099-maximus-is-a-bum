c0fba014fe6315d9861532cc74e433d57825f60d
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-03 08:06:17 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+import manual_functions.robot
+
+@TeleOp()
+public class main extends OpMode {
+    @Override
+    public void init() {
+        robot obj;
+        obj.initialize()
+    }
+    @Override
+    public void loop() {
+        
+    }
+}
