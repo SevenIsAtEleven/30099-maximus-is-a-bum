236db353e12ef6bd81bac8fed4c07e46412de7f7
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-03 13:20:13 -0700

    Update main.java

--- main.java
+++ main.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
     
 @TeleOp()
 public class main extends OpMode {
-    private robot obj;
+    private obj = new robot();
     
     @Override
     public void init() {
