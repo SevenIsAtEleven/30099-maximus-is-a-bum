3907a17847ab93ddfce348c0447ce963ae818063
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-04 12:00:20 -0700

    Update main.java

--- main.java
+++ main.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.robot;
 
 @TeleOp
 public class main extends LinearOpMode {
-    
+
     // External class implementation to make code neater
     private robot sys = new robot();
 
@@ -21,6 +21,7 @@ public class main extends LinearOpMode {
 
     //intake config
     private DcMotor extendo_motor;
+    private CRServo intake_servo;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -29,6 +30,7 @@ public class main extends LinearOpMode {
         motor_fr = hardwareMap.dcMotor.get("motor_f_r");
         motor_br = hardwareMap.dcMotor.get("motor_b_r");
 
+        intake_servo = hardwareMap.crservo.get("servo_intake");
         extendo_motor = hardwareMap.dcMotor.get("motor_extendo");
 
     /*while (opModeInInit()) {
@@ -50,5 +52,6 @@ public class main extends LinearOpMode {
     public void update() {
         sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo_motor);
+        sys.intake(gamepad2, intake_servo);
     }
 }
