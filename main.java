8d03051377444778c051dbf5cf97fc6bee5a2da2
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-03 17:56:19 -0700

    Update main.java

--- main.java
+++ main.java
@@ -18,6 +18,9 @@ public class main extends LinearOpMode {
     private DcMotor motor_fr;
     private DcMotor motor_br;
 
+    //intake config
+    private DcMotor h_slide_motor;
+
     //private DcMotor horizontal_slide_extension = hardwareMap.dcMotor.get("horizontal_slide")
 
     @Override
@@ -26,6 +29,8 @@ public class main extends LinearOpMode {
         motor_bl = hardwareMap.dcMotor.get("motor_b_l");
         motor_fr = hardwareMap.dcMotor.get("motor_f_r");
         motor_br = hardwareMap.dcMotor.get("motor_b_r");
+        
+        h_slide_motor = hardwareMap.dcMotor.get("hs_motor")
 
 	/*while (opModeInInit()) {
             this.funny_start_sequence();
@@ -41,35 +46,16 @@ public class main extends LinearOpMode {
         // Separate method implementation
         while (opModeIsActive()) {
             sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
+            sys.horizontal_slide(gamepad2, h_slide_motor)
         }
     }
-    /*public void drive() {
-        // y_val reversed
-        double y = -gamepad1.left_stick_y;
-
-        // Multiplier of 1.17 to counteract strafing range
-        double x = gamepad1.left_stick_x * 1.17;
-        double rx = gamepad1.right_stick_x;
-
-        // Range_limiter containes power within range [-1,1]
-        double range_limiter = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double power_fl = -1 * ((y + x + rx) / range_limiter);
-        double power_bl = -1 * ((y - x + rx) / range_limiter);
-        double power_fr = (y - x - rx) / range_limiter;
-        double power_br = (y + x - rx) / range_limiter;
-
-        // Set Power
-        motor_fl.setPower(power_fl);
-        motor_bl.setPower(power_bl);
-        motor_fr.setPower(power_fr);
-        motor_br.setPower(power_br);
-    }
+    /*
 
     //BETA!!!
     // funny start up sequence cos why not
     public void funny_start_sequence() {
-        horizontal_slide_extension.setPower(1);
+        h_slide_motor.setPower(0.5);
         Thread.sleep(750000000);
-        horizontal_slide_extension.setPower(1);
+        h_slide_motor.setPower(0.5;
     }*/
 }
