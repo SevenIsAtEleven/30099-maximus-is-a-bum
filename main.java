bbfc06b5e48dcfe23e47e8c570db11d1f4d0c5b3
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2025-07-05 16:06:45 -0700

    Update main.java

--- main.java
+++ main.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.robot;
 
 @TeleOp
 public class main extends LinearOpMode {
@@ -22,6 +23,9 @@ public class main extends LinearOpMode {
     private DcMotor extendo_motor;
     private CRServo intake_crservo;
 
+    private DcMotor lift_motor_l;
+    private DcMotor lift_motor_r;
+
     @Override
     public void runOpMode() throws InterruptedException {
         motor_fl = hardwareMap.dcMotor.get("motor_f_l");
@@ -31,6 +35,9 @@ public class main extends LinearOpMode {
 
         intake_crservo = hardwareMap.crservo.get("crservo_intake");
         extendo_motor = hardwareMap.dcMotor.get("motor_extendo");
+
+        lift_motor_l = hardwareMap.dcMotor.get("motor_lift_l");
+        lift_motor_r = hardwareMap.dcMotor.get("motor_lift_r");
 
     /*while (opModeInInit()) {
             sys.funny_start_sequence(h_slide_motor);
@@ -52,5 +59,6 @@ public class main extends LinearOpMode {
         sys.drive(gamepad1, motor_fl, motor_bl, motor_fr, motor_br);
         sys.extendo(gamepad2, extendo_motor);
         sys.intake(gamepad2, intake_crservo);
+        sys.lift(gamepad2, lift_motor_l,. lift_motor_r);
     }
 }
